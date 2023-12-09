0baea5fa450b5892dc2c7f2864aa08e3b14e7fc1
radioraccoon <radioraccoon@github.com>
2023-12-09 15:23:55 -0500

    - Initial test of RuneLite Plugin

--- src/main/java/com/tasklog/TaskLogPlugin.java
+++ src/main/java/com/tasklog/TaskLogPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.tasklog;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "TaskLog example"
 )
-public class ExamplePlugin extends Plugin
+public class TaskLogPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private TaskLogConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	TaskLogConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(TaskLogConfig.class);
 	}
 }
